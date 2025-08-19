1c8a08209854d446ee3bd7810c6f9fd5fb0da375
Kimberly <104057860+Kimberly2002@users.noreply.github.com>
2025-08-19 12:46:42 +0200

    Model TaskItem class populated

--- UserTasksApi/Models/TaskItem.cs
+++ UserTasksApi/Models/TaskItem.cs
@@ -2,5 +2,15 @@
 {
     public class TaskItem
     {
+        // Primary Key
+        public int Id { get; set; }   
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        // Foreign Key to User
+        public int AssigneeId { get; set; }
+        public User? Assignee { get; set; }
+
+        public DateTime DueDate { get; set; }
     }
 }
