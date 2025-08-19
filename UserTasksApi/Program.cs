802b5bd3d1f374b87952d192a350a87cae726662
Kimberly <104057860+Kimberly2002@users.noreply.github.com>
2025-08-19 15:47:04 +0200

    program class updated to configure DbContext and  migration files generated

--- UserTasksApi/Program.cs
+++ UserTasksApi/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using UserTasksApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Configure DbContext with SQLite
+builder.Services.AddDbContext<UserTasksContext>(options => options.UseSqlite("Data Source=UserTasks.db"));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
