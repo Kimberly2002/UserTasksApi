db7430df7e7760c0cd2f3bd8b172c709678a178b
Kimberly <104057860+Kimberly2002@users.noreply.github.com>
2025-08-19 15:03:13 +0200

    Data Context

---
+++ UserTasksApi/Data/UserTasksContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserTasksApi.Models;
+
+namespace UserTasksApi.Data
+{
+    public class UserTasksContext : DbContext
+    {
+        public UserTasksContext(DbContextOptions<UserTasksContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
