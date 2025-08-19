ce7fa221cdfb7978c6a803b9f1dfd6242486137e
Kimberly <104057860+Kimberly2002@users.noreply.github.com>
2025-08-19 23:59:20 +0200

    Repository classes created

---
+++ UserTasksApi/Repositories/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserTasksApi.Data;
+using UserTasksApi.Models;
+
+namespace UserTasksApi.Repositories
+{
+    
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserTasksContext _context;
+
+        public UserRepository(UserTasksContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User?> GetByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> AddAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(User user)
+        {
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
