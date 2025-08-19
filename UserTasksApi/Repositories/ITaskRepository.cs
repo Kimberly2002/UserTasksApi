7e3d704230920931d11bbc0e967d19af8515f57a
Kimberly <104057860+Kimberly2002@users.noreply.github.com>
2025-08-19 23:34:00 +0200

    task repository created.

---
+++ UserTasksApi/Repositories/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using UserTasksApi.Models;
+
+namespace UserTasksApi.Repositories
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<TaskItem>> GetAllAsync();
+        Task<TaskItem?> GetByIdAsync(int id);
+        Task<TaskItem> AddAsync(TaskItem task);
+        Task UpdateAsync(TaskItem task);
+        Task DeleteAsync(TaskItem task);
+    }
+}
