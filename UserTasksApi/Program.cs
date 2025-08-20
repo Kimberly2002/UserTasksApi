be1f4323a651147b550d547e4c5bf369d0c01df5
Kimberly <104057860+Kimberly2002@users.noreply.github.com>
2025-08-20 02:19:42 +0200

    bug fixed

--- UserTasksApi/Program.cs
+++ UserTasksApi/Program.cs
@@ -6,28 +6,30 @@ using UserTasksApi.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 // Configure DbContext with SQLite
-builder.Services.AddDbContext<UserTasksContext>(options => options.UseSqlite("Data Source=UserTasks.db"));
+builder.Services.AddDbContext<UserTasksContext>(options =>
+    options.UseSqlite("Data Source=UserTasks.db"));
 
+// Add Swagger + XML docs
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<ITaskRepository, TaskRepository>();
-
-
-var app = builder.Build();
-
 builder.Services.AddSwaggerGen(options =>
 {
     // Add XML comments path
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+    if (File.Exists(xmlPath))
+    {
+        options.IncludeXmlComments(xmlPath);
+    }
 });
+
+// Register repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
