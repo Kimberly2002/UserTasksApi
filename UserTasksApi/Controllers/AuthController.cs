7fc2e90c97a6708da5d4464ed4ecf8b46150dfe0
Kimberly <104057860+Kimberly2002@users.noreply.github.com>
2025-08-20 12:43:16 +0200

    used the proper api controller

--- UserTasksApi/Controllers/AuthController.cs
+++ UserTasksApi/Controllers/AuthController.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UserTasksApi.Controllers
 {
-    public class AuthController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
     }
 }
